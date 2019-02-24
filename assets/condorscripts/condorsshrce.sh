be0c6ff6caecbfe1a0afd7921ad2c18c7a34aa60
Shreyas Gadgin Matha <shreyas.gm61@gmail.com>
2019-02-24 12:35:59 -0500

    Updated condorscripts

--- assets/condorscripts/condorsshrce.sh
+++ assets/condorscripts/condorsshrce.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-INDEX=$(condor_q $1 -global -autoformat Args | grep -n "run_jupyter.sh" | cut -f1 -d:)
-SCHEDD=$(condor_q $1 -global -autoformat GlobalJobId | sed -n ''"$INDEX"'p' | cut -f1 -d#)
-ID=$(condor_q $1 -global -autoformat ClusterId | sed -n ''"$INDEX"'p')
+INDEX=$(condor_q username -global -autoformat Args | grep -n "run_jupyter.sh" | cut -f1 -d:)
+SCHEDD=$(condor_q username -global -autoformat GlobalJobId | sed -n ''"$INDEX"'p' | cut -f1 -d#)
+ID=$(condor_q username -global -autoformat ClusterId | sed -n ''"$INDEX"'p')
 echo "$SCHEDD"
 echo "$ID"
 condor_ssh_to_job -auto-retry -name "$SCHEDD" -ssh 'ssh -L 8889:localhost:8889 -o ServerAliveInterval=60' "$ID"
